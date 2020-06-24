43542f72fb3c27605efe2fe45c4d057627595a1d
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-06-23 23:35:24 -0400

    Test: respect existing _ keys for dupes

--- spec/dupekey_spec.lua
+++ spec/dupekey_spec.lua
@@ -6,45 +6,64 @@ local busted = require 'busted'
 local assert = require 'luassert'
 local yaml = require 'tinyyaml'
 
+local expected = {
+  map = {
+    Fruit = "apple",
+    Fruit_1 = "orange",
+    Fruit_2 = "banana",
+    Vegetable = "celery",
+    Vegetable_1 = "cucumber",
+    Vegetable_2 = "broccoli"
+  }
+}
+
 busted.describe("duplicate keys", function()
-  busted.it("preserves duplicate map keys in block style", function()
-    assert.same(
-      {
-        map = {
-          Fruit = "apple",
-          Fruit_1 = "orange",
-          Fruit_2 = "banana",
-          Vegetable = "celery",
-          Vegetable_1 = "cucumber",
-          Vegetable_2 = "broccoli"
-        }
-      },
-      yaml.parse([[
-        map:
-          Fruit : apple
-          Fruit : orange
-          Fruit : banana
-          Vegetable : celery
-          Vegetable : cucumber
-          Vegetable : broccoli
-      ]])
-    )
+  busted.describe("in block style", function()
+    busted.it("preserves duplicate map keys", function()
+      assert.same(
+        expected,
+        yaml.parse([[
+          map:
+            Fruit : apple
+            Fruit : orange
+            Fruit : banana
+            Vegetable : celery
+            Vegetable : cucumber
+            Vegetable : broccoli
+        ]])
+      )
+    end)
+    busted.it("recognizes existing _ keys", function()
+      assert.same(
+        expected,
+        yaml.parse([[
+          map:
+            Fruit   : apple
+            Fruit_1 : orange
+            Fruit   : banana
+            Vegetable   : celery
+            Vegetable_1 : cucumber
+            Vegetable   : broccoli
+        ]])
+      )
+    end)
   end)
-  busted.it("preserves duplicate map keys in flow style", function()
-    assert.same(
-      {
-        map = {
-          Fruit = "apple",
-          Fruit_1 = "orange",
-          Fruit_2 = "banana",
-          Vegetable = "celery",
-          Vegetable_1 = "cucumber",
-          Vegetable_2 = "broccoli"
-        }
-      },
-      yaml.parse([[
-        map: { Fruit: apple, Fruit: orange, Fruit: banana, Vegetable: celery, Vegetable: cucumber, Vegetable: broccoli }
-      ]])
-    )
+  busted.describe("in flow style", function()
+    busted.it("preserves duplicate map keys", function()
+      assert.same(
+        expected,
+        yaml.parse([[
+          map: { Fruit: apple, Fruit: orange, Fruit: banana, Vegetable: celery, Vegetable: cucumber, Vegetable: broccoli }
+        ]])
+      )
+    end)
+    busted.it("recognizes existing _ keys", function()
+      assert.same(
+        expected,
+        yaml.parse([[
+          map: { Fruit: apple, Fruit_1: orange, Fruit: banana, Vegetable: celery, Vegetable_1: cucumber, Vegetable: broccoli }
+        ]])
+      )
+    end)
   end)
 end)
